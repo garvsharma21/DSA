ebcaddb7a07c60102ed2fb6a99bf973f89a45ae6
garvsharma21 <garvsh7@gmail.com>
2026-02-18 00:11:10 +0530

    Made Optimizations to the last question of Phase2 Level4

--- Phase2/Level4/num_pyramid.py
+++ Phase2/Level4/num_pyramid.py
@@ -4,17 +4,17 @@ def num_pyramid(num):
     if num<=0:
         return -1
     
-    n = 5
     for row in range(0, num):
+        n = 6
         for col in range(0, num-row-1):
-            print(" ", end='')
+            print(" ", end=' ')
         for col in range(0, 2*row + 1):
-            print(n, end=' ')
-            if col <= ((row + col) // 2):
-                n = n + 1
-            else:
+            if col <= row:
                 n = n - 1
-            
+            else:
+                n = n + 1  
+            print(n, end=' ')
+
         print()
 
 num = int(input("Enter a positive number: "))
